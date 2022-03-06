84b57d67df315f3afee70ff44ec5719f252cc585
farouk2001 <farouksherif62@gmail.com>
2022-03-06 20:04:25 +0200

    Initial commit

---
+++ lib/theme_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyThemes{
+  static ThemeData lightTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      colorScheme: ColorScheme.light(),
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.white,
+      )
+  );
+  static ThemeData darkTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.black,
+      colorScheme: ColorScheme.dark(),
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.black,
+      )
+  );
+}
